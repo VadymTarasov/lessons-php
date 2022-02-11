9463075546bc4455d972a8fe5d6751c3ee6c5950
vadym <vadym.tarasov.91@gmail.com>
2022-02-11 20:03:59 +0200

    задание 3

---
+++ task-3.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ Используя функцию rand() и switch case вывести
+   - Если число равно 1, 2, 3 ,4, 5 - Odd
+   - Если число равно 10, 20, 30, 40, 50 - Even
+   - Иначе - Ooops
+*/
+
+$a = rand();
+
+switch ($a) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        echo "Odd " . $a;
+        break;
+    case 10:
+    case 20:
+    case 30:
+    case 40:
+    case 50:
+        echo "Even " . $a;
+        break;
+    default:
+        echo "Ooops " . $a;
+}
