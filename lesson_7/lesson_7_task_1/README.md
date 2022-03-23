92c0a1bf4f07cebabd7bce5eb91f9439a729d90f
vadym <vadym.tarasov.91@gmail.com>
2022-03-23 14:44:20 +0200

    лекция 7 задание 1

---
+++ lesson_7/lesson_7_task_1/README.md
@@ -0,0 +1,24 @@
+
+#[![Typing SVG](https://readme-typing-svg.herokuapp.com?color=%2336BCF7&lines=Привет!)](https://git.io/typing-svg)
+
+![](https://github.com/blackcater/blackcater/raw/main/images/Hi.gif)
+
+Функция выведет на экран содержимое каталога
+====
+Описание работы функции
+----
+
+1. Переменная $dir принимает аргументом строку абсолютный путь к каталогу, который необходимо просканировать.
+2. Получаем полный список файлов и каталогов [scandir($dir)](https://www.php.net/manual/ru/function.scandir.php)
+3. В переменной $path получаем полный путь к файлу.
+
+```php
+ $path = $dir.'/'.$file;
+```
+
+4. Определяем, является ли имя файла директорией [is_dir($path)](https://www.php.net/manual/ru/function.is-dir.php)
+5. Если это директория, выводим название директории, делая заданный отступ $space.
+6. С помощью рекурсии выводим содержание полученной директории.
+7. Если это файл, то просто выводим название файла. 
+8. Функции  [fileperms](https://www.php.net/manual/ru/function.fileperms.php) и [filesize](https://www.php.net/manual/ru/function.filesize.php) отображают информацию о правах на файл и размер файла.
+
---
+++ lesson_7/lesson_7_task_1/lesson_7_task_1.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Написать функцию, которая рекурсивно выведет на экран содержимое каталога.
+Функция принимает аргументом строку абсолютный путь к каталогу,
+который необходимо просканировать.
+
+d - директория
+f - файл
+
+отсупы необходимо сохранить, чтобы при отображении было понятно структуру вложенности
+/var/www/data
+--------------
+d public
+  d assets
+    f style.css
+    f script.js
+d templates
+  f index.html
+f index.php
+f composer.json
+f composer.lock
+* Дополнительная задача со звездочкой. Используя табуляцию (отступы) отобразить права файла (в формате 644) и занимаемый размер на диске по файлу/каталогу (байты/килобайты)
+PS. Аргументы скрипт должен принимать с CLI. Например:
+
+php index.php '/var/www/data'*/
+
+$dir = '/var/www/lessons-php/lesson_7/lesson_7_task_1';
+
+function viewDir($dir, $space)
+{
+    $files = scandir($dir);
+    foreach ($files as $file) {
+        if (($file == '.') || ($file == '..')) continue;
+        $path = $dir . '/' . $file;
+        if (is_dir($path)) {
+            $x = substr(sprintf('%o', fileperms($path)), -3);
+            echo '<b>' . $space . "d " . $file . "&nbsp;&nbsp;&nbsp;&nbsp; права -" . substr(sprintf('%o', fileperms($path)), -3). '&nbsp;&nbsp;&nbsp;&nbsp;размер - '  . filesize($path) . ' байтов' ."</b><br />";
+
+            viewDir($path, $space . '&nbsp;&nbsp;');
+        } else {
+            echo $space . "f " . $file . "&nbsp;&nbsp;&nbsp;&nbsp; права -" . substr(sprintf('%o', fileperms($path)), -3) .'&nbsp;&nbsp;&nbsp;&nbsp;размер - '  . filesize($path) . ' байтов'."<br />";
+        }
+    }
+}
+
+viewDir($dir, '');
+
+
+
+
+
+
+
+
