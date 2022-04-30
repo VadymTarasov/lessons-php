8b1fe202a1748405b8c9b379710e1787bef6fdf3
Vadym <vadym.tarasov.91@gmail.com>
2022-04-30 16:12:19 +0300

    добавил кнопку Ничья

--- lesson_14/lesson_14_task_2/index.php
+++ lesson_14/lesson_14_task_2/index.php
@@ -17,17 +17,19 @@ if (iconv_strlen($_SESSION['allGameCity']) > 150) {
 if ($_SESSION['copyCity']) echo $_SESSION['copyCity'];
 
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+              crossorigin="anonymous">
 
-    <title>Игра в города</title>
-  </head>
-  <body>
+        <title>Игра в города</title>
+    </head>
+    <body>
 
     <div class="container">
         <div class="offset-md-2 w-50">
@@ -37,23 +39,31 @@ if ($_SESSION['copyCity']) echo $_SESSION['copyCity'];
 
                 <div class="input-group input-group-sm mb-3">
                     <span class="input-group-text" id="userCity">Введите город</span>
-                    <input type="text" name="userCity" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                    <input type="text" name="userCity" class="form-control" aria-label="Sizing example input"
+                           aria-describedby="inputGroup-sizing-sm">
                 </div>
 
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Отправить</button>
+                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                    <button type="submit" class="btn btn-success">Отправить</button>
+                    <button type="submit" id="giveUp" name="giveUp" value="Ничья. " class="btn btn-warning">Предложить
+                        ничью
+                    </button>
                 </div>
-
                 <div class="mb-6">
                     <label for="gameCity">Ответ компьютера</label>
-                    <input name="gameCity" value="<?=$_SESSION['gameCity']; ?>" type="text" class="form-control" id="gameCity">
+                    <input name="gameCity" value="<?= $_SESSION['gameCity']; ?>" type="text" class="form-control"
+                           id="gameCity">
                 </div>
-
-                  <div class="mb-3">
+                <div class="mb-3">
                     <label for="gameCity">Результат игры</label>
                     <textarea name="gameCity" class="form-control" id="gameCity"><?php
-                        if ($_SESSION['allGameCity']){echo$_SESSION['allGameCity'];}
-                        else echo "Начните игру!"
+
+                        if ($_COOKIE['giveUp']) {
+                            echo $_COOKIE['giveUp'];
+                        }
+                        if ($_SESSION['allGameCity']) {
+                            echo $_SESSION['allGameCity'];
+                        } else echo "Начните игру!"
                         ?>
                     </textarea>
                 </div>
@@ -62,17 +72,18 @@ if ($_SESSION['copyCity']) echo $_SESSION['copyCity'];
 
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
-</html>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
+    </body>
+    </html>
 
 <?php
 if ($_SESSION['allGameCity'] == 'Победила дружба!') {
     session_unset();
     session_destroy();
 }
-if (iconv_strlen($_SESSION['copyCity']) > 1)
-{
+if (iconv_strlen($_SESSION['copyCity']) > 1) {
     session_unset();
     session_destroy();
 }
--- lesson_14/lesson_14_task_2/result.php
+++ lesson_14/lesson_14_task_2/result.php
@@ -6,14 +6,24 @@ require_once 'Db.php';
 require_once 'Game.php';
 session_start();
 
+var_dump($_POST);
+
 header('Location: /lesson_14/lesson_14_task_2/index.php');
 
+if ($_POST['giveUp'])
+{
+    session_unset();
+    session_destroy();
+}
+
+setcookie("giveUp", "{$_POST['giveUp']}", time() + 10, '/');
 
 $userCity = Game::textCheck($_POST['userCity']);
-var_dump($userCity);
 $test2 = new Db();
 $cityAllName = $test2->getAllCities();
 $test = new Game($cityAllName, $userCity);
+
+
 $gameCity = $test->getAnswer();
 $_SESSION['allGameCity'] .= $gameCity . " -> ";
 $_SESSION['gameCity'] = $gameCity;
