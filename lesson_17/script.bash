3bf1298ca5f810bd61e9ced3ee2ca531541f6343
Vadym <vadym.tarasov.91@gmail.com>
2022-05-10 19:27:13 +0300

    Урок 17. ДЗ 13.

--- lesson_17/script.bash
+++ lesson_17/script.bash
@@ -3,13 +3,20 @@
 #1. Змінити розширення файлу. Скрипт приймає як параметри ім'я файлу та нове розширення.
 # Відобразити старе і нове ім’я файлів.
 
+echo "Введите имя файла"
+read Nane
+echo "Введите новое расширение"
+read Format
+
 myFunction() {
   a=$1
-  echo "старе ім’я $1"
+  echo "Старое имя $1"
   b=`echo $a | cut -d. -f 1`
   c=$2
   d="."
   e="$b$d$c"
-  echo "нове ім’я $e"
+  echo "Новое имя $e"
+  mv $a $e
+
 }
-myFunction test.txt php
+myFunction "$Nane" "$Format"
